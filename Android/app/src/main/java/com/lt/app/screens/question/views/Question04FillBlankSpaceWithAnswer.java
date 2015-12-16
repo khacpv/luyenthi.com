53ebbb2dae41be7a0751ab12ceb8dba0cf793713
khacpham <khacpv@gmail.com>
2015-12-16 13:29:14 +0700

    make full screen

--- Android/app/src/main/java/com/lt/app/screens/question/views/Question04FillBlankSpaceWithAnswer.java
+++ Android/app/src/main/java/com/lt/app/screens/question/views/Question04FillBlankSpaceWithAnswer.java
@@ -2,7 +2,9 @@ package com.lt.app.screens.question.views;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 
+import com.lt.app.R;
 import com.lt.app.common.view.question.QuestionLayout;
 
 /**
@@ -10,15 +12,24 @@ import com.lt.app.common.view.question.QuestionLayout;
  * Display question with fill answer into blank space. have answers to choice
  */
 public class Question04FillBlankSpaceWithAnswer extends QuestionLayout {
+
     public Question04FillBlankSpaceWithAnswer(Context context) {
         super(context);
+        init();
     }
 
     public Question04FillBlankSpaceWithAnswer(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public Question04FillBlankSpaceWithAnswer(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init(){
+        LayoutInflater.from(getContext()).inflate(R.layout.question_layout_04_fillblankspacewithanswer,this,true);
+
     }
 }
