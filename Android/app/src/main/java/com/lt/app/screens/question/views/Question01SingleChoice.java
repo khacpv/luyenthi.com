7491af709315d5d4b522bcb4ab4d5e1ba9244d95
EO_CUONG <hoangcuong200693@gmail.com>
2015-12-16 13:52:53 +0700

    coding QuestionSingleChoice

--- Android/app/src/main/java/com/lt/app/screens/question/views/Question01SingleChoice.java
+++ Android/app/src/main/java/com/lt/app/screens/question/views/Question01SingleChoice.java
@@ -2,8 +2,11 @@ package com.lt.app.screens.question.views;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.FrameLayout;
 
+import com.lt.app.R;
 import com.lt.app.common.view.question.QuestionLayout;
 
 /**
@@ -14,14 +17,25 @@ public class Question01SingleChoice extends QuestionLayout {
     public static final String TAG = Question01SingleChoice.class.getName();
 
     public Question01SingleChoice(Context context) {
+
         super(context);
+        init();
     }
 
     public Question01SingleChoice(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public Question01SingleChoice(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
+    }
+
+
+    void init() {
+            View v= LayoutInflater.from(getContext()).inflate(R.layout.question_layout_01_singlechoice,this,true);
+
+
     }
 }
